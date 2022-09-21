ddf119f1432f15aef55ae95783f0cd3a25a92ecf
Dan Henderson <sftwrngnr66@gmail.com>
2022-09-21 16:17:06 -0700

    Set time offset to elapsed seconds since oth record.

--- xlatedata.py
+++ xlatedata.py
@@ -5,16 +5,24 @@ import numpy
 
 
 fnames = [ "data/UERecode1.txt"] # "data/velodyne_points.csv",
-timeOff = -53806943784960.0
+timeOff = -53806943784960.0 #probably not needed anymore
+zeroTimeVal = -1
 
 def getCoord(tField):
     return float(tField[2:])
 
 def getTime(tField):
+    global zeroTimeVal
     tf = tField.split(":")
     myTime = tf[1] + ":" + tf[2] + ":" + tf[3]
     myDate = tf[1][:-3]
-    testTime = numpy.longdouble(time.mktime(datetime.datetime.strptime(myTime, "%Y-%m-%d %H:%M:%S").timetuple()) * 10e8) + timeOff
+    testTime = time.mktime(datetime.datetime.strptime(myTime, "%Y-%m-%d %H:%M:%S").timetuple())
+    if (zeroTimeVal == -1):
+        zeroTimeVal = testTime
+        testTime = 0 #Initial time value is zeroed out
+    else:
+        #We need to return the number of elapsed seconds
+        testTime = testTime - zeroTimeVal
     return testTime
 
 
@@ -24,7 +32,7 @@ def proc_uer_line(instr):
     x = getCoord(fields[2])
     y = getCoord(fields[3])
     z = 0.0
-    return("%16.0f,%5.2f,%5.2f,%4.2f" %(cTime , x ,y ,z))
+    return("%16.0f,%f,%f,%f" %(cTime , x ,y ,z))
 
 
 for nm in fnames:
