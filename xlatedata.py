8a5de152f2d97f4c2cb9d5ffe683f5d1ab96b4c1
Dan Henderson <sftwrngnr66@gmail.com>
2022-09-21 13:00:04 -0700

    Updated xlatedata.py

--- xlatedata.py
+++ xlatedata.py
@@ -5,25 +5,26 @@ import numpy
 
 
 fnames = [ "data/UERecode1.txt"] # "data/velodyne_points.csv",
-#timeOff = 
+timeOff = -53806943784960.0
+
+def getCoord(tField):
+    return float(tField[2:])
 
 def getTime(tField):
     tf = tField.split(":")
     myTime = tf[1] + ":" + tf[2] + ":" + tf[3]
     myDate = tf[1][:-3]
-    print(myTime)
-    testTime = numpy.longdouble(time.mktime(datetime.datetime.strptime(myTime, "%Y-%m-%d %H:%M:%S").timetuple()) * 10e8)
-    print( time.mktime(datetime.datetime.strptime(myTime, "%Y-%m-%d %H:%M:%S").timetuple()))
-    print( time.mktime(datetime.datetime.strptime(myDate, "%Y-%m-%d").timetuple()))
-    print(testTime)
-    print(1616037858056215080.0)
-    print (1616037858056215080.0 - testTime)
+    testTime = numpy.longdouble(time.mktime(datetime.datetime.strptime(myTime, "%Y-%m-%d %H:%M:%S").timetuple()) * 10e8) + timeOff
+    return testTime
 
 
 def proc_uer_line(instr):
     fields = instr.split(",")
-    print(fields[0])
-    getTime(fields[0])
+    cTime = getTime(fields[0])
+    x = getCoord(fields[2])
+    y = getCoord(fields[3])
+    z = 0.0
+    return("%16.0f,%5.2f,%5.2f,%4.2f" %(cTime , x ,y ,z))
 
 
 for nm in fnames:
@@ -31,7 +32,6 @@ for nm in fnames:
     print()
     with open(nm, "r") as f:
         for inln in f:
-            print(inln[:-1])
-            proc_uer_line(inln[:-1])
+            print(proc_uer_line(inln[:-1]))
 
 
